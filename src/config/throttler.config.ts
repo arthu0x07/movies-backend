b239b59b13701b181e54dda2e89e6188f11dc3f6
Arthur Silva <arthur.santos.off@gmail.com>
2025-05-26 19:50:49 -0300

    feat(throttler): refactor throttler configuration to use an array structure for multiple throttlers

--- src/config/throttler.config.ts
+++ src/config/throttler.config.ts
@@ -1,6 +1,10 @@
 import { ThrottlerModuleOptions } from '@nestjs/throttler'
 
 export const throttlerConfig: ThrottlerModuleOptions = {
-  ttl: 60, // tempo em segundos
-  limit: 10, // número máximo de requisições no período
-} 
+  throttlers: [
+    {
+      ttl: 60, // tempo em segundos
+      limit: 10, // número maximo de requisições
+    },
+  ],
+}
